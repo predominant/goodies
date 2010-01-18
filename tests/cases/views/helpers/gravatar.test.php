9643300f7a2fb84fad2552459340b50013bf29bb
predominant <graham@grahamweldon.com>
2010-01-18 21:46:46 +1100

    Cleanup Unit Tests. Change Test Class name and add documentation.

--- tests/cases/views/helpers/gravatar.test.php
+++ tests/cases/views/helpers/gravatar.test.php
@@ -1,29 +1,65 @@
 <?php
+App::import('Core', array('ClassRegistry', 'Controller', 'Helper', 'AppHelper', 'View', 'Security'));
+App::import('Helper', array('Html', 'Goodies.Gravatar'));
+
 /**
  * CakePHP Gravatar Helper Test
  *
- * @copyright Copyright 2009, Graham Weldon
+ * @copyright Copyright 2010, Graham Weldon
  * @license http://www.opensource.org/licenses/mit-license.php The MIT License
- * @author jadb
  * @package goodies
  * @subpackage goodies.tests.cases.helpers
+ *
  */
-App::import('Core', array('ClassRegistry', 'Controller', 'Helper', 'AppHelper', 'View', 'Security'));
-App::import('Helper', array('Html', 'Goodies.Gravatar'));
-class UsabilityHelperTest extends CakeTestCase {
+class GravatarHelperTest extends CakeTestCase {
+
+/**
+ * Gravatar helper
+ *
+ * @var GravatarHelper
+ * @access public
+ */
 	public $Gravatar = null;
+
+/**
+ * Start Test
+ *
+ * @return void
+ * @access public
+ */
 	public function startTest() {
 		$this->Gravatar =& ClassRegistry::init('GravatarHelper');
 		$this->Gravatar->Html =& ClassRegistry::init('HtmlHelper');
 	}
+
+/**
+ * End Test
+ *
+ * @return void
+ * @access public
+ */
 	public function endTest() {
 		unset($this->Controller);
 		unset($this->Gravatar);
 	}
+
+/**
+ * testCorrectInstances
+ *
+ * @return void
+ * @access public
+ */
 	public function testCorrectInstances() {
 		$this->assertIsA($this->Gravatar, 'GravatarHelper');
 		$this->assertIsA($this->Gravatar->Html, 'HtmlHelper');
 	}
+
+/**
+ * testUrl
+ *
+ * @return void
+ * @access public
+ */
 	public function testUrl() {
 		$expected = 'http://www.gravatar.com/avatar/' . Security::hash('example@gravatar.com', 'md5');
 		$result = $this->Gravatar->url('example@gravatar.com', array('ext' => false));
