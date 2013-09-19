2cbc46c74cfc0b7f57b41eb49e58500be9eb8fbc
Graham Weldon <graham@grahamweldon.com>
2013-09-19 15:14:01 +0900

    Fix for inclusion in various situations and locations

--- View/Helper/AutoJavascriptHelper.php
+++ View/Helper/AutoJavascriptHelper.php
@@ -65,17 +65,35 @@ class AutoJavascriptHelper extends AppHelper {
 			$this->request->controller . '.js',
 			$this->request->controller . DS . $this->request->action . '.js');
 
+		$checkFiles = array();
+
 		foreach ($files as $file) {
 			$file = $path . $file;
+
 			if ($theme && !empty($this->theme)) {
-				$includeFile = VIEWS . 'themed' . DS . $this->theme . DS . 'webroot' . DS . 'js' . DS . $file;
-			} else {
-				$includeFile = JS . $file;
+				$checkFiles[] = 'theme' . DS . $this->theme . DS . 'js' . DS . $file;
 			}
-			if (file_exists($includeFile)) {
-				$file = str_replace('\\', '/', $file);
-				$this->Html->script($file, array('inline' => false));
+			$checkFiles[] = array(JS . $file, $file);
+		}
+
+		foreach ($checkFiles as $file) {
+			if ($this->findAndInclude($file)) {
+				break;
 			}
 		}
+	}
+
+	protected function findAndInclude($file) {
+		if (is_array($file)) {
+			list($file, $includeFile) = $file;
+		} else {
+			$includeFile = $file;
+		}
+		if (file_exists($file)) {
+			$includeFile = str_replace('\\', '/', $includeFile);
+			$this->Html->script($includeFile, array('inline' => false));
+			return true;
+		}
+		return false;
 	}
 }
