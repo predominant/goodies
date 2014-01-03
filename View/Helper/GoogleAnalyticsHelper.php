189d537ae5505a302ebddec519e2baa84485b211
Graham Weldon <graham@grahamweldon.com>
2014-01-03 12:45:35 +0900

    Enabled Google Analytics Universal style

--- View/Helper/GoogleAnalyticsHelper.php
+++ View/Helper/GoogleAnalyticsHelper.php
@@ -20,12 +20,23 @@ class GoogleAnalyticsHelper extends AppHelper {
  *
  * @return string Html output for code snippet
  */
-	public function pageTracker($account) {
-		return '<script>
+	public function pageTracker($account, $domain = null, $universal = true) {
+		if ($universal) {
+			return "<script>
+	(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+	(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+	m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+	})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+	ga('create', '" . $account . "', '" . $domain . "');
+	ga('send', 'pageview');
+</script>";
+		} else {
+			return '<script>
 	var _gaq=[["_setAccount","' . $account . '"],["_trackPageview"]];
 	(function(d,t){var g=d.createElement(t),s=d.getElementsByTagName(t)[0];g.async=1;
 	g.src=("https:"==location.protocol?"//ssl":"//www")+".google-analytics.com/ga.js";
 	s.parentNode.insertBefore(g,s)}(document,"script"));
 </script>';
+		}
 	}
 }
